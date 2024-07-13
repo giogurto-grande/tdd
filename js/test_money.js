d81bd7a9f104d8d7365b99e5d11a7da3b818d2b5
Kamil Kozłowski <kamil.kozlowski@cdex.cloud>
2024-07-13 18:24:26 +0200

    addition

--- js/test_money.js
+++ js/test_money.js
@@ -15,12 +15,31 @@ class Money {
     }
 }
 
+class Portfolio {
+    constructor(){
+        this.moneys = [];
+    }
+
+    add(...moneys) {
+        this.moneys = this.moneys.concat(moneys);
+    }
+
+    evaluate(currency) {
+        let total = this.moneys.reduce(
+            (sum, money) => {
+                return sum + money.amount;
+            }, 0
+        );
+        return new Money(total, 'USD');
+    }
+}
+
 let fiveDollars = new Money(5, 'USD');
 let tenDollars = new Money(10, 'USD');
 
 assert.deepStrictEqual(fiveDollars.times(2), tenDollars);
 
-let tenEuros= new Money(10, 'EUR');
+let tenEuros = new Money(10, 'EUR');
 let twentyEuros = new Money(20, 'EUR');
 
 assert.deepStrictEqual(tenEuros.times(2), twentyEuros);
@@ -29,3 +48,9 @@ let originalMoney = new Money(4002, 'KRW');
 let expectedMoneyAfterDivision = new Money(1000.5, 'KRW');
 
 assert.deepStrictEqual(originalMoney.divide(4), expectedMoneyAfterDivision);
+
+let fifteenDollars = new Money(15, 'USD');
+let portfolio = new Portfolio();
+portfolio.add(fiveDollars, tenDollars);
+assert.deepStrictEqual(portfolio.evaluate('USD'), fifteenDollars);
+
